3eb00a8f9ef381888c6a56df0e9b9254ec340690
Aden Aziz Hasnayakin <aden.aziz948@gmail.com>
2022-06-15 17:04:34 +0700

    init

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const port = 3000;
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
-  // cors: {
-  //   origin: ["http://localhost:3000", "http://127.0.0.1:5500", "http://10.0.2.2", "http://10.0.2.2:3000"],
-  //   methods: ["GET", "POST"],
-  // },
+  cors: {
+    origin: ["http://localhost:3000", "http://127.0.0.1:5500", "http://10.0.2.2", "http://10.0.2.2:3000"],
+    methods: ["GET", "POST"],
+  },
 });
 
 app.use(express.json());
@@ -20,8 +20,6 @@ app.post("/send_sms", (req, res) => {
 
   // * Emit SMS to device Id.
   const deviceId = req.body.device_id;
-
-  console.log(deviceId);
   io.to(deviceId).emit("sendSMS", req.body);
 
   return res.json({ ...req.body, status: "SENT" });
